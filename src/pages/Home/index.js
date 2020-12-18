570f4a0ed010bf507e20c47e8cd8d35f82192b6a
Melanie Salzman <salzmanmelanie@gmail.com>
2020-12-18 08:36:48 -0300

    Implement Home Page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+import Banner from '../../components/Banner'
+import CategoryList from '../../components/CategoryList'
+import './styles.css'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <Banner />
+            <CategoryList />
+        </div>
+    )
+}
+
+export default Home
